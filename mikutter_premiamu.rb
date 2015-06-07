bf96f707502d63b03ab4d00e76a0759946ce0f00
ahiru <wellingtonnmaeda0524@gmail.com>
2015-06-07 19:02:09 +0900

    Not working properly

--- mikutter_premiamu.rb
+++ mikutter_premiamu.rb
@@ -4,12 +4,6 @@
 
    DEFINED_TIME = Time.new.freeze
 
-   def reply_and_favorite(tweet)
-     replySentence = "puremiamuじゃなくてpremiamuだよ"
-     Service.primary.post(:message => "#{"@" + tweet.user.idname + ' ' + replySentence}", :replyto => tweet)
-     tweet.message.favorite(true)
-   end
-
    on_appear do |ms|
 
      #does_reply_self_message = UserConfig[:does_reply_self_message] || false
@@ -18,9 +12,9 @@
        if m.message.to_s =~ /puremiamu/ and m[:created] > DEFINED_TIME and !m.retweet?
          unless m.me # and does_reply_self_message
            replySentence = "puremiamuじゃなくてpremiamuだよ"
-		   Service.primary.post(:message => "#{"@" + tweet.user.idname + ' ' + replySentence}", :replyto => tweet)
-		   tweet.message.favorite(true)
-		   # reply_and_favorite(m)
+           Service.primary.post(:message => "#{"@" + tweet.user.idname + ' ' + replySentence}", :replyto => tweet)
+           tweet.message.favorite(true)
+           # reply_and_favorite(m)
          end
        end
      end
