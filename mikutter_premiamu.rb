b1fa6893ed1987f920aaebb30a39c02e9ca84245
ahiru <wellingtonnmaeda0524@gmail.com>
2015-06-08 11:06:03 +0900

    なぜ動かない

--- mikutter_premiamu.rb
+++ mikutter_premiamu.rb
@@ -6,11 +6,13 @@ Plugin.create(:ahiru_yakuna) do
 
   on_appear do |ms|
     ms.each do |m|
-      if m.message.to_s =~ /puremiamu/ and m[:created] > DEFINED_TIME and !m.retweet?
-      Service.primary.post(:message => "#{"@" + m.user.idname + ' puremiamuじゃなくてpremiamuだよ'}", :replyto => m)
-      m.message.favorite(true)
+      unless m.me?
+        if m.message.to_s =~ /puremiamu/ and m[:created] > DEFINED_TIME and !m.retweet?
+        Service.primary.post(:message => "#{"@" + m.user.idname + ' puremiamuじゃなくてpremiamuだよ'}", :replyto => m)
+        m.message.favorite(true)
+        end
+      end
      end
     end
   end
-end
 
