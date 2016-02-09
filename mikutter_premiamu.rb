c8986c5f5cc6f45ea726cf9afc8e6d8cd24182d2
ahiru <ahiru@ahiru-MBP.local>
2016-02-09 19:15:30 +0900

    test commit

--- mikutter_premiamu.rb
+++ mikutter_premiamu.rb
@@ -2,17 +2,16 @@
 
 Plugin.create(:ahiru_yakuna) do
 
-  DEFINED_TIME = Time.new.freeze
-
+  PREMIAMU_DEFINED_TIME = Time.new.freeze
   on_appear do |ms|
     ms.each do |m|
-      unless m.me?
-        if m.message.to_s =~ /puremiamu/ and m[:created] > DEFINED_TIME and !m.retweet?
-        Service.primary.post(:message => "#{"@" + m.user.idname + ' puremiamuじゃなくてpremiamuだよ'}", :replyto => m)
-        m.message.favorite(true)
-        end
+    #   unless Service.primary.to_user
+      if m.message.to_s =~ /premium/ and m[:created] > PREMIAMU_DEFINED_TIME and !m.retweet? and m.user.idname != Service.primary.to_user
+        Service.primary.post(:message => "#{"@" + m.user.idname + ' premiumじゃなくてpremiamuだよ'}", :replyto => m)
+        # end
       end
-     end
     end
   end
+
+end
 
